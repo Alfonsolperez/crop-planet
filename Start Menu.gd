6bae1982fe7cec9c1968224b62a85ed2ae97033e
GGGalang <paologgalang@gmail.com>
2024-05-18 23:40:09 +0800

    main menu

--- Start Menu.gd
+++ Start Menu.gd
@@ -3,13 +3,18 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	$VBoxContainer/StartBtn.grab_focus() # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
-
 func _on_start_btn_pressed():
 	get_tree().change_scene_to_file("res://cropplanet.tscn") # Replace with function body.
+
+func _on_exit_btn_pressed():
+	get_tree().quit() # Replace with function body.
+
+func _on_options_btn_pressed():
+	get_tree().change_scene_to_file("res://Options.tscn") # Replace with function body.
