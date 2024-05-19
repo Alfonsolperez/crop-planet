2a0e92e0ad34e711f3579106d7220977758a50b3
Ethan-west <fatavora@cbzrc.pshs.edu.ph>
2024-05-19 15:35:15 +0800

    updated cloud a bit use arrowkeys

--- scripts/Cloud.gd
+++ scripts/Cloud.gd
@@ -3,7 +3,6 @@ extends Node2D
 var d := 0.0
 var radius := 250.0
 var speed := 2
-var direction := 1.0
 var animalskilled = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,28 +10,18 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
+	var input_axis = -Input.get_axis("ui_left", "ui_right")
+	spin(delta,input_axis)
+	
+func spin(delta,input_axis):
 	if speed != 0:
-		d += delta * direction
+		d += delta * input_axis
 	var viewport_center = get_viewport_rect().size / 2
-	if speed == 2:
-		position = viewport_center + Vector2(
-			sin(d * speed) * radius,
-			cos(d * speed) * radius
+	position = viewport_center + Vector2(
+		sin(d * speed) * radius,
+		cos(d * speed) * radius
 		)
 	
 	var angle = atan2(viewport_center.y - position.y, viewport_center.x - position.x)
 	rotation = angle - PI / 2  # Adding PI/2 to make bottom face the center
-func _input(event):
-	if Input.is_action_pressed("move_counterclockwise"):
-		direction = 1.0
-	elif Input.is_action_pressed("move_clockwise"):
-		direction = -1.0
-	elif Input.is_action_pressed("move_stop"):
-		if speed == 0:
-			speed = 2
-		else:
-			speed = 0
-		
-	
-
