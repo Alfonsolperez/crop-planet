5899fa4c98d12731b32d0d984686e0148d463dcb
wvanterola <wvanterola@cbzrc.pshs.edu.ph>
2024-05-19 13:26:41 +0800

    Changed the Fonts

--- Earth.gd
+++ Earth.gd
@@ -3,11 +3,11 @@ extends AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	center_object()
+	pass
 
 # Called every frame. Alternatively, you can use _physics_process for physics-related updates.
 func _process(delta):
-	center_object()
+	pass
 
 func center_object():
 	var viewport_size = get_viewport_rect().size
--- Start Menu.gd
+++ Start Menu.gd
@@ -3,7 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$VBoxContainer/StartBtn.grab_focus() # Replace with function body.
+	$BtnContainer/StartBtn.grab_focus() # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
